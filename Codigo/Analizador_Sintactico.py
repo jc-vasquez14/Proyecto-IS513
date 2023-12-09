1c7266cf09fbbe2d882308564f84f8aef5a7be20
IIIIIIIIIIIII <128424012+J0Ss3@users.noreply.github.com>
2023-12-08 23:59:42 -0600

    Update Analizador_Sintactico.py

--- Codigo/Analizador_Sintactico.py
+++ Codigo/Analizador_Sintactico.py
@@ -1,6 +1,6 @@
 import ply.yacc as yacc
 import ast
-from Analizador_Lexico import tokens  # Import tokens from your lexical analyzer
+from Analizador_Lexico import tokens  #Importar token del analizador lexico
 
 
 # Define the grammar rules
@@ -39,13 +39,17 @@ def p_end(p):
 
 def p_position(p):
     '''
-    position : POSICION NUMERO NUMERO
+    position : POSICION 
     '''
     p[0] = (p[1], p[2], p[3])
 
-# Error rule for syntax errors
+# Error para errores de sintaxis (syntax errors)
 def p_error(p):
-    print("Syntax error in input!")
+    print("SyntaxError!")
+
+
+#Crear el analizadpr
+parser = yacc.yacc()
 
 
 
